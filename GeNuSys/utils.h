2ca456ccafc9fd125d3610962b3279eb3db86473
numsys <numsys@sygnm.org>
2017-09-04 12:23:56 +0200

    added GENUSYS_VERSION_{MAJOR,MINOR} #defines

--- GeNuSys/utils.h
+++ GeNuSys/utils.h
@@ -1,6 +1,9 @@
 #ifndef GENUSYS_UTILS_H_
 #define GENUSYS_UTILS_H_
 
+#define GENUSYS_VERSION_MAJOR 1
+#define GENUSYS_VERSION_MINOR 1
+
 #ifndef NDEBUG
 #define ASSERT_EXCEPTION(x,y) if(!(x)) throw y{#x}
 #else
